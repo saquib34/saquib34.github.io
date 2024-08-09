e5f7e2ce0079bdfcd13ebd3800b95361176462e6
saquib34 <shadmanshahin6@gmail.com>
2024-08-09 14:16:18 +0530

    Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './component/navbar';
 
 function App() {
