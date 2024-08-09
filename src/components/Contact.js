35300fc9dcac563cfef95d9e017996bf23cad19b
saquib34 <shadmanshahin6@gmail.com>
2024-08-10 01:51:12 +0530

    Website Complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,7 @@ import { FiUser, FiMail, FiMessageSquare, FiSend } from 'react-icons/fi';
 
 
 const apiKey = process.env.REACT_APP_WEB3FORMS_API_KEY;
+console.log(apiKey);
 
 function Contact() {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
